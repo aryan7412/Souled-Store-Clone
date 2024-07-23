cec0583981423f94f22d79abcf77b7aef9b8ffd5
aryan7412 <aryan74bs@gmail.com>
2024-07-23 21:49:54 +0530

    FinalHomepage

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Mens from './components/Mens';
+import Women from './components/Women';
+import Kids from './components/Kids';
+function App() {
+  return (
+    <>
+    {/* <Women/> */}
+    <Mens />
+    {/* <Kids /> */}
+    </>
+  );
+}
+
+export default App
+
